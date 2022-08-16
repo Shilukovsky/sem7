8299ca072bc0911e6df28cac4cef79098c68e8c5
Nikolay <cccp_ussr@bk.ru>
2022-08-16 07:37:55 +0300

    added Z49

---
+++ Z49/Program.cs
@@ -0,0 +1,46 @@
+﻿int[,] GetMatrix (int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+int[,] GetSquare (int[,] matrixA)
+{
+    for (int i = 0; i < matrixA.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixA.GetLength(1); j++)
+        {
+            if(i % 2 == 0)
+            if(j % 2 == 0)
+                matrixA[i,j] = matrixA[i,j] * matrixA[i,j];
+        }
+    }
+    return matrixA;
+}
+
+void PrintMatrix(int[,] array)
+{   for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(j == 0) Console.Write("[");
+            if(j < array.GetLength(1) - 1) Console.Write($"{array[i,j], 3},");
+            else Console.Write($"{array[i,j], 3}]");
+        }
+        Console.WriteLine();
+    }    
+}
+
+int[,] matrixResult = GetMatrix(3,4);
+PrintMatrix(matrixResult);
+Console.WriteLine();
+int[,] matrixAResult = GetSquare(matrixResult);
+PrintMatrix(matrixAResult);
